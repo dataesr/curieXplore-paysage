aa1c860aa6913a4900175823e32de56533b1da12
jerem1508 <jeremy.peglion@gmail.com>
2021-05-07 09:50:45 +0200

    add cors in staging branch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './config';
 import loadCountries from './controllers/load-countries';
 import findCountry from './controllers/paysage';
 
 const app = express();
+app.use(cors());
 app.get('/api/load-countries', loadCountries);
 app.get('/api/paysage/:id', findCountry);
 app.get('/api/', (req, res) => res.json({ message: "Welcome to curiexplore-api" }));
