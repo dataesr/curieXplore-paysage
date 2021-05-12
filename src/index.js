74ef8e201a2fddaaec1aa0bc3b66780c2b2eb409
paulinegc <francasix@outlook.com>
2021-05-12 11:45:27 +0200

    express port 3000 by default

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import loadCountries from './controllers/load-countries';
 import findCountry from './controllers/paysage';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(cors());
 app.get('/api/load-countries', loadCountries);
 app.get('/api/paysage/:id', findCountry);
 app.get('/api/', (req, res) => res.json({ message: "Welcome to curiexplore-api" }));
 
-app.listen(process.env.PORT, () => {
-  console.log(`App is listening on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
 });
