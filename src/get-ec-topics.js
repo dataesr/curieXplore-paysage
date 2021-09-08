516d1222058858c6288224e101f0df29e93a838a
folland87 <frederic@olland.me>
2021-09-08 16:36:40 +0200

    fix(topics): fix ec-topics route

--- src/get-ec-topics.js
+++ src/get-ec-topics.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const URL = 'https://ec.europa.eu/info/funding-tenders/opportunities/data/topicDetails/';
 export default async function getEcTopics(req, res) {
   const { id } = req.params;
-  const headers = { 'Access-Control-Allow-Origin': '*' };
+  const headers = { 'Access-Control-Allow-Origin': '*', Accept: 'application/json' };
   const topics = await axios.get(`${URL}${id}.json`, { headers });
-  res.setHeaders('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ topics });
+  res.set('Access-Control-Allow-Origin', '*');
+  res.status(200).json(topics.data.TopicDetails);
 }
